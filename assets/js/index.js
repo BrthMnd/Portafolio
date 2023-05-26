f568ca3a16e64e7297699672d2bb815eea5081ae
Brandon Quintero <developerbvth@gmail.com>
2023-05-26 04:27:49 +0200

    Nuevo Portafolio Orientado a React

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,34 @@
 
-const WindownsOfProjectsdate = () =>{
+const WindownsOfProjectsPortafolio = () =>{
     Swal.fire({
         title: 'Portafolio',
-        html: '<img src="../assets/img/Captura.PNG" width="400px" height="200px"><p class="alert_p">Portafolio Actual</p> ',
+        html: '<a href="#img__presentation"><img src="../assets/img/Portafolio.png" width="450px" height="200px"></a> <p class="alert_p">Portafolio Actual</p> ',
         
       })
 
 }
-const WindownsOfProjectsAnime = () =>{
+const WindownsOfProjectsTaskTierList = () =>{
     Swal.fire({
-        title: 'AnimeFLV',
-        html: '<img src="../assets/img/captura_anime.png" width="450px" height="250px"> <p class="alert_p">Pagina web de anime provisional</p> ',
+        title: 'TaskTierList',
+        html: '<a href="https://brthmnd.github.io/TaskTierList/"><img src="../assets/img/TaskTierlist.png" width="450px" height="200px"></a> <p class="alert_p">Pagina para listar tareas</p>',
       })
 
 }
-const WindownsOfProjectsColor = () =>{
+const WindownsOfProjectsCrud = () =>{
     Swal.fire({
         title: 'ColorHunt',
         text: 'Modal with a custom image.',
-        html: '<img src="../assets/img/captura_colorhunt.png" width="450px" height="250px"> <p class="alert_p">Pagina web provisional que se utiliza para escoger paletas de colores para paginas web</p> ',
+        html: '<a href="https://crudbrandon-bvg0.onrender.com/aprendices"><img src="../assets/img/crud.png" width="450px" height="200px"></a> <p class="alert_p">Pagina web que administra aprendices y sus notas</p> ',
       })
 
 }
 
 
 let btn = document.getElementById('img__pagina__date')
-btn.addEventListener("click",WindownsOfProjectsdate)
+btn.addEventListener("click",WindownsOfProjectsPortafolio)
 
 let btn2 = document.getElementById('img__anime__date')
-btn2.addEventListener("click",WindownsOfProjectsAnime)
+btn2.addEventListener("click",WindownsOfProjectsTaskTierList)
 
 let btn3 = document.getElementById('img__color__date')
-btn3.addEventListener("click",WindownsOfProjectsColor)
+btn3.addEventListener("click",WindownsOfProjectsCrud)
